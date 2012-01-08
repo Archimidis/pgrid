b5f6e3ad2b81b9d0060a6ffc89e66bfe7c5bceac
Nikolas Vourlakis <nvourlakis@gmail.com>
2012-01-09 00:32:51 +0200

    FixNode algorithm implementation.
    The implementation is based on the thesis protocol and is accompanied with
    some unit tests. I also added the closestHosts method to class
    RoutingTable to help to the algorithm.
    Copyright update to all the files by IDEA ide.

---
+++ test/pgrid/entity/routingtable/RoutingTableClosestHostTest.java
@@ -0,0 +1,169 @@
+package pgrid.entity.routingtable;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import pgrid.entity.Host;
+import pgrid.entity.internal.PGridHost;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Test cases for method {@link RoutingTable#closestHosts(String)}.
+ *
+ * @author Nikolas Vourlakis <nvourlakis@gmail.com>
+ */
+public class RoutingTableClosestHostTest {
+
+    private static Host localhost_;
+
+    @BeforeClass
+    public static void beforeClass() throws UnknownHostException {
+        localhost_ = new PGridHost("127.0.0.1", 3000);
+        localhost_.setHostPath("000");
+    }
+
+    @Test
+    public void WhenSearchMatchLocalhost_ExpectLocalhost() throws UnknownHostException {
+        String SEARCH = "000"; // == "000"
+
+        Host host1 = new PGridHost("127.0.0.1", 1111);
+        host1.setHostPath("1");
+        Host host2 = new PGridHost("127.0.0.1", 2222);
+        host2.setHostPath("01");
+        Host host3 = new PGridHost("127.0.0.1", 3333);
+        host3.setHostPath("0010");
+        Host host4 = new PGridHost("127.0.0.1", 4444);
+        host4.setHostPath("0011");
+
+        RoutingTable routingTable = new RoutingTable();
+        routingTable.setLocalhost(localhost_);
+
+        routingTable.addReference(0, host1);
+        routingTable.addReference(1, host2);
+        routingTable.addReference(2, host3);
+        routingTable.addReference(2, host4);
+
+        Collection<Host> actual = routingTable.closestHosts(SEARCH);
+
+        Assert.assertTrue(actual.size() == 1);
+        Assert.assertTrue(actual.contains(localhost_));
+
+        Assert.assertFalse(actual.contains(host1));
+        Assert.assertFalse(actual.contains(host2));
+        Assert.assertFalse(actual.contains(host3));
+        Assert.assertFalse(actual.contains(host4));
+    }
+
+    @Test
+    public void WhenSearchPrefixOfLocalhost_ExpectLocalhost() throws UnknownHostException {
+        String SEARCH = "00"; // prefix of "000"
+
+        Host host1 = new PGridHost("127.0.0.1", 1111);
+        host1.setHostPath("1");
+        Host host2 = new PGridHost("127.0.0.1", 2222);
+        host2.setHostPath("01");
+        Host host3 = new PGridHost("127.0.0.1", 3333);
+        host3.setHostPath("0010");
+        Host host4 = new PGridHost("127.0.0.1", 4444);
+        host4.setHostPath("0011");
+
+        RoutingTable routingTable = new RoutingTable();
+        routingTable.setLocalhost(localhost_);
+
+        routingTable.addReference(0, host1);
+        routingTable.addReference(1, host2);
+        routingTable.addReference(2, host3);
+        routingTable.addReference(2, host4);
+
+        Collection<Host> actual = routingTable.closestHosts(SEARCH);
+
+        Assert.assertTrue(actual.size() == 1);
+        Assert.assertTrue(actual.contains(localhost_));
+
+        Assert.assertFalse(actual.contains(host1));
+        Assert.assertFalse(actual.contains(host2));
+        Assert.assertFalse(actual.contains(host3));
+        Assert.assertFalse(actual.contains(host4));
+    }
+
+    @Test
+    public void WhenSearchNotPrefixOfLocalhost_ExpectHostFromConjugateTree() throws UnknownHostException {
+        String SEARCH = "1010101"; // no prefix relation to "000"
+
+        Host host1 = new PGridHost("127.0.0.1", 1111);
+        host1.setHostPath("1");
+
+        RoutingTable routingTable = new RoutingTable();
+        routingTable.setLocalhost(localhost_);
+
+        routingTable.addReference(0, host1);
+
+        Collection<Host> actual = routingTable.closestHosts(SEARCH);
+
+        Assert.assertTrue(actual.size() == routingTable.getLevel(0).size());
+        Assert.assertTrue(actual.contains(host1));
+
+        Assert.assertFalse(actual.contains(localhost_));
+    }
+
+    @Test
+    public void WhenSearchIsPrefixAndLevelsNotEmpty_ExpectHostCollection() throws UnknownHostException {
+        String SEARCH = "00110"; // prefix relation "000" and level for path "001" not empty
+
+        Host host1 = new PGridHost("127.0.0.1", 1111);
+        host1.setHostPath("1");
+        Host host2 = new PGridHost("127.0.0.1", 2222);
+        host2.setHostPath("01");
+        Host host3 = new PGridHost("127.0.0.1", 3333);
+        host3.setHostPath("0010");
+        Host host4 = new PGridHost("127.0.0.1", 4444);
+        host4.setHostPath("0011");
+
+        RoutingTable routingTable = new RoutingTable();
+        routingTable.setLocalhost(localhost_);
+
+        routingTable.addReference(0, host1);
+        routingTable.addReference(1, host2);
+        routingTable.addReference(2, host3);
+        routingTable.addReference(2, host4);
+
+        Collection<Host> actual = routingTable.closestHosts(SEARCH);
+
+        Assert.assertTrue(actual.size() == routingTable.getLevel(2).size());
+        Assert.assertTrue(actual.contains(host3));
+        Assert.assertTrue(actual.contains(host4));
+
+        Assert.assertFalse(actual.contains(localhost_));
+        Assert.assertFalse(actual.contains(host1));
+        Assert.assertFalse(actual.contains(host2));
+    }
+
+    @Test
+    public void WhenSearchIsPrefixButLevelEmpty_ExpectHostCollectionInPrefixRelation() throws UnknownHostException {
+        String SEARCH = "00110"; // prefix relation "000" but level for path "001" is empty
+
+        Host host1 = new PGridHost("127.0.0.1", 1111);
+        host1.setHostPath("1");
+        Host host2 = new PGridHost("127.0.0.1", 2222);
+        host2.setHostPath("01");
+
+        RoutingTable routingTable = new RoutingTable();
+        routingTable.setLocalhost(localhost_);
+
+        routingTable.addReference(0, host1);
+        routingTable.addReference(1, host2);
+
+        Collection<Host> expected = new ArrayList<Host>(1);
+        expected.add(localhost_);
+
+        Collection<Host> actual = routingTable.closestHosts(SEARCH);
+
+        Assert.assertTrue(actual.contains(localhost_));
+
+        Assert.assertFalse(actual.contains(host1));
+        Assert.assertFalse(actual.contains(host2));
+    }
+}
